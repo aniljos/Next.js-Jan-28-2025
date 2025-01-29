464fecc3a85e1c3d93eb4e3ff96579212848b46f
aniljos <anil.jos@gmail.com>
2025-01-29 12:16:00 +0530

    Day 2 Session 1

--- the-awesome-app/src/app/layout.tsx
+++ the-awesome-app/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+import { AppBar } from "@/components/AppBar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,25 +30,8 @@ export default function RootLayout({
 
         <div className="container">
 
-          <nav className="navbar navbar-dark bg-dark">
-            <div className="container-fluid">
-              <Link className="navbar-brand" href="/">Next.js</Link>
-              <ul className="nav">
-                <li className="nav-item">
-                  <Link className="nav-link" href="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/about">About</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/login">Login</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/products">Products</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+          <AppBar/>
+          
           <main>
             {children}
           </main>
--- the-awesome-app/src/app/login/page.tsx
+++ the-awesome-app/src/app/login/page.tsx
@@ -1,8 +1,59 @@
-export default function LoginPage(){
+'use client'
+
+import { ChangeEvent, FormEvent, useState } from "react"
+
+export default function LoginPage() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+
+    function handleSubmit(evt: FormEvent<HTMLFormElement>){
+
+        evt.preventDefault();
+
+        if(!username || !password){
+           setMessage("Enter the credentials");
+        }
+        else{
+            // API call to validate the creds
+            setMessage("");
+        }
+
+    }
+
+    function handleUsernameChange(evt: ChangeEvent<HTMLInputElement>){
+        
+        const value = evt.target.value;
+        setUsername(value)
+       
+    }
+
     return (
         <div>
             <h3>Login</h3>
             <p>Signin to the application here...</p>
+
+            {message ? <div className="alert alert-danger">Enter the credentials</div> : null}
+
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" className="form-control" id="username" value={username} onChange={handleUsernameChange}/>
+                    {/* <span>You entered {username}</span> */}
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="pwd">Password</label>
+                    <input type="password" className="form-control" id="pwd" value={password} 
+                                                                onChange={evt => setPassword(evt.target.value)}/>
+                </div>
+
+                <br />
+
+                <button className="btn btn-primary">Login</button>
+            </form>
         </div>
     )
 }
